c0750dbdab93997e54f4464b7e2548c0d6340e7f
Angie Natalia <Nathiz0210@gmail.com>
2019-05-26 19:20:42 -0500

    inicio sesión con acceso a pantalla registro y viseversa

--- app/src/main/java/co/edu/unisabana/arquitectura/proyectoprueba/Register.java
+++ app/src/main/java/co/edu/unisabana/arquitectura/proyectoprueba/Register.java
@@ -1,10 +1,15 @@
 package co.edu.unisabana.arquitectura.proyectoprueba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
@@ -19,6 +24,22 @@ public class Register extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.opciones, android.R.layout.simple_spinner_item);
 
         opciones.setAdapter(adapter);
+
+
+
+        final EditText correo = (EditText) findViewById(R.id.correo);
+        final EditText contrasena = (EditText) findViewById(R.id.contrasena);
+        final Spinner tipo = (Spinner) findViewById(R.id.spinner);
+        final TextView iniciolink= (TextView) findViewById(R.id.inicio);
+        final CardView registro = (CardView) findViewById(R.id.registro);
+
+        iniciolink.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(Register.this, MainActivity.class);
+                Register.this.startActivity(registerIntent);
+            }
+        });
     }
 }
 
