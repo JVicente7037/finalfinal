0c37b129908419e7d03a0a4770d63bc70fafb128
Angie Natalia <Nathiz0210@gmail.com>
2019-05-26 22:03:02 -0500

    Pantalla inicio sesión y registro terminadas, con logo y clases de Otras interfaces vacias

---
+++ app/src/main/java/co/edu/unisabana/arquitectura/proyectoprueba/MenuConductor.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.arquitectura.proyectoprueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuConductor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_conductor);
+    }
+}
